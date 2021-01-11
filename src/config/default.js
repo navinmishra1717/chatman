17fbb82e745f931e0d9110828a9d3ac5058b9be3
navinmishra1717 <navinmishra1717@gmail.com>
2021-01-11 10:16:02 +0545

    implementing and verifying dotenv module

--- src/config/default.js
+++ src/config/default.js
@@ -3,7 +3,7 @@
 /* eslint no-undef: 0 */ // For allowing process.env
 
 module.exports = {
-  apiPort: process.env.apiPort || 3018,
+  apiPort: process.env.API_PORT || 3018,
   debugLogStream: process.env.debugLogStream || process.stdout,
   infoFile: process.env.infoLogFile || "src/log/server.log",
   writtenBy: process.env.writtenBy || "abdian",
